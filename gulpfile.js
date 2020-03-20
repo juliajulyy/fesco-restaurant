a0f8732fbabce9638363d64acbabf1dba92a2d97
Yulia Cheliak <bluetownbird@gmail.com>
2020-03-20 19:56:42 +0200

    fix style bugs & improve gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,13 @@ gulp.task('browser-sync', function() {
 	});
 });
 
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
 gulp.task('watch', function() {
 	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')); 
+	gulp.watch('app/*.html', gulp.parallel('code'));
 });
 gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
