fb81a8149904a2c0375eba4a7e6f20378fe38322
Yulia Cheliak <bluetownbird@gmail.com>
2020-03-21 17:36:29 +0200

    add new configurates

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,63 @@
-var gulp 				= require('gulp'),
-		sass 				= require('gulp-sass'),
-		browserSync = require('browser-sync');
+const gulp 				= require('gulp'),
+			sass 				= require('gulp-sass'),
+			browserSync = require('browser-sync'),
+			imagemin 		= require('gulp-imagemin'),
+			cleanDir 		= require('gulp-clean-dir'),
+			cssMin 			= require('gulp-cssmin');
 
-gulp.task('sass', function(){
-	return gulp.src('app/scss/**/*.scss')
+gulp.task('sass', () =>
+	gulp.src('app/scss/**/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('app/css'))
+		.pipe(cssMin())
+		.pipe(gulp.dest('dist/css'))
 		.pipe(browserSync.reload({stream: true}))
-});
+);
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', () =>
 	browserSync({
 		server: {
-			baseDir: 'app'
+			baseDir: 'dist'
 		},
 		notify: false
-	});
-});
+	})
+);
 
-gulp.task('code', function() {
-	return gulp.src('app/*.html')
-	.pipe(browserSync.reload({ stream: true }))
-});
+gulp.task('html', () => 
+	gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.reload({stream: true}))
+);
 
-gulp.task('watch', function() {
+gulp.task('img-min', () =>
+	gulp.src('app/img/*')
+	  .pipe(imagemin())
+	  .pipe(gulp.dest('dist/img'))
+	  .pipe(browserSync.reload({stream: true}))
+);
+
+gulp.task('font', () =>
+	gulp.src('app/fonts/*')
+	  .pipe(gulp.dest('dist/fonts'))
+	  .pipe(browserSync.reload({stream: true}))
+);
+
+gulp.task('watch', () => {
 	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')); 
-	gulp.watch('app/*.html', gulp.parallel('code'));
+	gulp.watch('app/*.html', gulp.parallel('html'));
 });
-gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+
+gulp.task('cleanDir', () =>
+	gulp.src('dist', {read: false})
+  	.pipe(cleanDir('./dist'))
+);
+
+const defaultTask = () => gulp.parallel(
+	'sass', 
+	'img-min',
+	'font',
+	'html', 
+	'browser-sync', 
+	'watch'
+);
+
+gulp.task('default', gulp.series('cleanDir', defaultTask()));
